83b5c27d7e86b2698f6ac1d2be8fda3e018b14ff
Katelyn Maher <katieamaher@yahoo.com>
2021-04-28 10:57:45 -0500

    commit on master

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-      //variables
-      //let computerChoice = ;
+//variables
       //let playerChoice = ;
       //let result = ;
-      let randomNum = Math.round(Math.random() * (3));
-      console.log(randomNum);
+      let randomNum = Math.round(Math.random() * (2));
 
     // The computer's turn 
-    //function computerPlay() {
-
-    //}
+    function computerPlay() {
+      if (randomNum === 0) {
+        return "rock";
+      }
+      else if (randomNum === 1) {
+        return "paper";
+      }
+      else if (randomNum === 2) {
+        return "scissors";
+      }
+    }
 
     // One game of rock, paper, sissors 
     //function playRound() {
