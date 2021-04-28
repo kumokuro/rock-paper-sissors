478850d4a8429e3dce9d3e0745c5c4794fbadaee
Katelyn Maher <katieamaher@yahoo.com>
2021-04-28 09:43:10 -0500

    Add script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+      //variables
+      //let computerChoice = ;
+      //let playerChoice = ;
+      //let result = ;
+      let randomNum = Math.round(Math.random() * (3));
+      console.log(randomNum);
+
+    // The computer's turn 
+    //function computerPlay() {
+
+    //}
+
+    // One game of rock, paper, sissors 
+    //function playRound() {
+
+    //}
